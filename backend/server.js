d23be07f3342dabb8b79c8dacff367d743fcebb0
haja2611 <haja0786308@gmail.com>
2025-05-06 17:12:33 +0530

    XCLONE-MyFirstRoute

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+//const express =  require('express')
+import express from 'express';
+import dotenv from 'dotenv'
+import authRoute from './routes/auth.route.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT; 
+
+app.use("/api/auth", authRoute);
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT}`);
+})
+
+
