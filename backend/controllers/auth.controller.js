e2a1a09c83552355860a4a4f3538b36627e530ae
haja2611 <haja0786308@gmail.com>
2025-05-12 16:55:58 +0530

    X-Clone SignOut

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+export const signup =  (req, res)=>{
+   try{
+    const {username, fullname, email, password} = req.body;
+   }catch(error){
+    console.log(`Error in signup controller: ${error}`)
+res.status(500).json({error: "Internal server Error"})   
+}
+}
+export const login =  (req, res)=>{
+    res.send("login");
+}
+export const logout =  (req, res)=>{
+    res.send("logout");
+}
+
+
+
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-//const express =  require('express')
+
 import express from 'express';
 import dotenv from 'dotenv'
 import authRoute from './routes/auth.route.js';
-
+//import connectDB from './db/connectDB.js';
+import connectDB from './db/connectDB.js'
+ 
 dotenv.config();
 
 const app = express();
@@ -11,7 +13,8 @@ const PORT = process.env.PORT;
 app.use("/api/auth", authRoute);
 
 app.listen(PORT, ()=>{
-    console.log(`server is running on ${PORT}`);
+    console.log(`server is running on ${PORT}`)
+    connectDB();
 })
 
 
